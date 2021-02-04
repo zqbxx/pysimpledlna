15e21ec874ee478abd48a0a57a2395ce6f3d0b3f
zqbxx <3808964+zqbxx@users.noreply.github.com>
2021-02-04 09:55:52 +0800

    优化命令行界面输出

--- pysimpledlna/ui/terminal.py
+++ pysimpledlna/ui/terminal.py
@@ -1,8 +1,9 @@
 from sys import stdout
 import os
 from enum import Enum
-
-from prompt_toolkit.key_binding import KeyBindings
+import shutil
+from wcwidth import wcwidth
+from prompt_toolkit import prompt
 
 
 class PlayerStatus(Enum):
@@ -22,11 +23,33 @@ class Player:
         self._player_status = PlayerStatus.STOP
         self._cur_pos = 0
 
+        self.pause = False
+
+
     def draw(self):
+
+        columns, lines = shutil.get_terminal_size((80, 20))
+
         text = '\r'
         text += self.format_time(self.cur_pos) + '/' + self.format_time(self.duration) + ' | '
         text += self.player_status.value
-        text += ' current: ' + self._video_file_name
+        text += ' current: '
+        text_columns = sum(wcwidth(c) for c in text)
+        remain_columns = columns - text_columns
+        if remain_columns > 0:
+            name_columns = sum(wcwidth(c) for c in self._video_file_name)
+            if name_columns <= remain_columns:
+                text += self._video_file_name
+                text += ' ' * (remain_columns - name_columns)
+            else:
+                # 剩余空间太小就不显示
+                if remain_columns > 5:
+                    # 截取头部和尾部显示
+                    part_len = int((remain_columns-2)/2)
+                    text += self._video_file_name[:part_len-1] + '..' + self._video_file_name[-part_len+1:]
+                else:
+                    text += ' ' * (remain_columns - name_columns)
+
         stdout.write(text)
 
     def format_time(self, seconds) -> str:
@@ -72,8 +95,9 @@ if __name__ == '__main__':
     import time
     play_ui = Player()
     play_ui.duration = 1000
-    play_ui.video_file = 'D:/test/test.mkv'
+    play_ui.video_file = 'D:/test/abcedfghijklmnopqrstuvwxyz-abcedfghijklmnopqrstuvwxyz-abcedfghijklmnopqrstuvwxyz-abcedfghijklmnopqrstuvwxyz-abcedfghijklmnopqrstuvwxyz-abcedfghijklmnopqrstuvwxyz.mkv'
+
     for i in range(1000):
         time.sleep(1)
-        play_ui.cur_pos = i +1
+        play_ui.cur_pos = i + 1
         play_ui.draw()
