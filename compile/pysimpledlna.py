791fb2c25f541df2948a2fa79f7e8b6f55efbe70
zqbxx <3808964+zqbxx@users.noreply.github.com>
2021-06-13 10:56:51 +0800

    更新编译脚本

--- compile/pysimpledlna.py
+++ compile/pysimpledlna.py
@@ -1,3 +1,13 @@
+import sys
+import os
+
+exe_dir = sys.path[1]
+lib_dir = os.path.join(exe_dir, 'lib')
+dll_dir = os.path.join(exe_dir, 'dll')
+sys.path.append(lib_dir)
+os.environ["PATH"] = dll_dir + os.pathsep + os.environ["PATH"]
+os.environ["nuitka"] = '0'
+os.environ["nuitka_exe_dir"] = exe_dir
 from pysimpledlna.cli import main
 
 if __name__ == "__main__":
