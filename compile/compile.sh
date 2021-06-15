284c6f05da1deddf99d4e2d3b9c727be9f1bdfc1
zqbxx <3808964+zqbxx@users.noreply.github.com>
2021-06-15 10:25:02 +0800

    调整pyd文件位置

--- compile/compile.sh
+++ compile/compile.sh
@@ -7,6 +7,6 @@ echo $output_dir
 python -m nuitka --mingw64 --show-progress --standalone --output-dir=$output_dir --nofollow-import-to=pygments --nofollow-import-to=PIL --nofollow-import-to=pytest --nofollow-import-to=OpenSSL --nofollow-import-to=service_identity --nofollow-import-to=prompt_toolkit --nofollow-import-to=Bottle --nofollow-import-to=cryptography ./pysimpledlna.py
 mkdir -p $output_dir/pysimpledlna.dist/pysimpledlna/templates
 cp -R ./pysimpledlna/templates $output_dir/pysimpledlna.dist/pysimpledlna
-cp ./pysimpledlna/bottle.*.pyd $output_dir/pysimpledlna.dist/
-cp ./pysimpledlna/prompt_toolkit.*.pyd $output_dir/pysimpledlna.dist/
+cp ./bottle.*.pyd $output_dir/pysimpledlna.dist/
+cp ./prompt_toolkit.*.pyd $output_dir/pysimpledlna.dist/
 rm -rf $output_dir/pysimpledlna.build
